69000cccfec5c09f1c89eddaa5e5107428e4629a
edgarsilva <edgar.osc@gmail.com>
2019-02-14 19:01:22 -0600

    Redux Setup - First reducer, auth reducer and passing it to the store.

--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-
 // import './index.css';
+
 import App from './components/App';
+import reducers from './reducers';
+
 // import * as serviceWorker from './serviceWorker';
 
-
-const store = createStore(() => [], {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware());
 
 ReactDOM.render(
   <Provider store={store}><App /></Provider>,
