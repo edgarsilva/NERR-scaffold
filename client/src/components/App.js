f36c3c4a05d00e27d6d984b7bd74f42367a89ec2
edgarsilva <edgar.osc@gmail.com>
2019-02-14 18:49:09 -0600

    Basic Redux Setup - Provider, Store and middleware, no reducers yet.

---
+++ client/src/components/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>Hi There!</div>
+  )
+};
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h2>Welcome to REACT!</h2>
+//           <p>
+//             Edit <code>src/App.js</code> and save to reload.
+//           </p>
+//           <a
+//             className="App-link"
+//             href="https://reactjs.org"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             Learn React
+//           </a>
+//           <a
+//             className="App-link"
+//             href="/auth/google"
+//           >
+//             Sign In with Google
+//           </a>
+//         </header>
+//       </div>
+//     );
+//   }
+// }
+
+export default App;
--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+
+// Require models
+require('./models/User');
+
+// Require services
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
+
+// Creating Express app.
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there', bye: 'buddy'});
-});
+// Setting-Up Middleware
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60* 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Setting up Auth Routes
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
